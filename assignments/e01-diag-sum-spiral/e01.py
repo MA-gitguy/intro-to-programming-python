137f21d349f546cfdca2826f1ba41b5737635f8f
MA-gitguy <maabdullahse@gmail.com>
2025-08-23 22:09:31 +0500

    comments added

--- assignments/e01-diag-sum-spiral/e01.py
+++ assignments/e01-diag-sum-spiral/e01.py
@@ -8,9 +8,9 @@ def get_diag_sum(n):
     if n<2 or n%2==0:
         return None
 
-    diag_sum = 1
-    diag_num_add = 2
-    diag_num = 1
+    diag_sum = 1 # final solution will be in this variable
+    diag_num_add = 2 # each rotation has specific value (3, 5, 7, 9) or (13, 17, 21, 25) diff of 2 and 4 respectively
+    diag_num = # this is to track the number value and add it to final solution within loop
 
     num = n // 2
 
@@ -18,7 +18,7 @@ def get_diag_sum(n):
         for _ in range(4):
             diag_num += diag_num_add
             diag_sum += diag_num
-        diag_num_add += 2
+        diag_num_add += 2 # as one rotation complete the values get double
 
     return diag_sum
 if __name__ == '__main__':
